3d296055c82799a6a1212226078fc91a83f26d95
sumoondev <sumoon.dev@gmail.com>
2026-05-05 11:02:59 +0545

    feat: Create plane and grid

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,43 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(1, 10, 10);
+orbit.update();
+
+const planeGeometry = new THREE.PlaneGeometry(30,30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffffff,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry,planeMaterial);
+scene.add(plane);
+plane.rotation.x = - Math.PI / 2;   
+
+const gridHelper = new THREE.GridHelper(30,20);
+scene.add(gridHelper);
+
+function animate() {
+    renderer.render(scene,camera);
+}
+
+renderer.setAnimationLoop(animate);
